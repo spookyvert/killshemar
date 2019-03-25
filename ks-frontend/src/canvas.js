dc7d2786031388e177abb1c55cee26e72a6a5aa5
github username <github email address>
2019-03-25 19:20:52 -0400

    real double jump, player bounds and a portal

--- ks-frontend/src/canvas.js
+++ ks-frontend/src/canvas.js
@@ -1,10 +1,12 @@
 let spr1;
 let GRAVITY = 0.8;
 let GROUND_Y = 350;
-let JUMP = -8; // how powerful is jump?
+let JUMP = -8;
 let img;
 let platformSpr;
 let platformSwitch;
+let jumpSwitch = true;
+let jumpCount = 0;
 
 const p1 = {
   x: 400,
@@ -20,7 +22,12 @@ const platform1 = {
   h: 20
 }
 
-
+const portal = {
+  x: 604,
+  y: 390,
+  w: 25,
+  h: 25
+}
 
 function randomDirection() {
   return (Math.floor(Math.random() * 2) == 0) ? platformSwitch = true : platformSwitch = false;
@@ -28,22 +35,27 @@ function randomDirection() {
 
 randomDirection()
 
-
 function preload() {
   img = loadImage('assets/grass.png');
 }
 
 function setup() {
   createCanvas(800, 400);
+  fill(0, 255, 0)
+  portalSpr = createSprite(portal.x, portal.y, portal.w, portal.h)
+
+
   let r = random(650);
+
+
 
   spr1 = createSprite(
     p1.x, p1.y, p1.w, p1.h);
   spr1.shapeColor = color(255, 0, 0);
   spr1.velocity.y = 0;
 
+  fill(0, 200, 0)
   platformSpr = createSprite(r, 330, 70, 20)
-
 
 }
 
@@ -70,6 +82,16 @@ function draw() {
     spr1.velocity.y = 0
   }
 
+//limits jumping to 2 consecutive jumps
+  if (spr1.position.y >= 370){
+    jumpCount = 0
+    jumpSwitch = true
+  }
+
+
+  if (spr1.position.y >= 390){
+    spr1.position.y = 390;
+  }
 
   if (platformSwitch === true) {
     if (platformSpr.position.x >= 810) {
@@ -78,34 +100,42 @@ function draw() {
 
       platformSpr.position.x += 1.5;
     }
-  } else {
+  }
+  else {
     if (platformSpr.position.x <= 80) {
       platformSwitch = true
-    } else {
-
+    }
+    else {
       platformSpr.position.x -= 1.5
     }
   }
-
 
   spr1.collide(platformSpr)
 
 
 
-  if (keyIsDown(RIGHT_ARROW)) {
-
+  if (keyIsDown(RIGHT_ARROW) && spr1.position.x < 790) {
     spr1.position.x += 5;
-
-
-  } else if (keyIsDown(LEFT_ARROW)) {
+  }
+  else if (keyIsDown(LEFT_ARROW) && spr1.position.x > 10) {
     spr1.position.x -= 5;
-
-  } else if (keyIsDown(UP_ARROW) && spr1.position.y > 330) {
-    // p1.y -= 100
-    spr1.velocity.y = JUMP;
-  } else if (key == ' ') {
-    // spr1.setSpeed(0, 0);
   }
   return false;
+}
 
+function keyPressed(){
+  if (keyIsDown(UP_ARROW) && jumpSwitch) {
+    if (jumpCount >= 1){
+      jumpSwitch = false
+    }
+    else {
+    spr1.velocity.y = JUMP;
+    jumpCount++
+    }
+  }
+  else if (keyIsDown(DOWN_ARROW) && spr1.position.x >= 602 && spr1.position.x <= 607 && spr1.position.y === 390) {
+    spr1.position.x = 200
+    spr1.position.y = 200
+  }
+  return false;
 }
