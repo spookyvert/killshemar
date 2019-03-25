bb12f02607d84a715a4a27822df83b8868125b53
github username <github email address>
2019-03-25 15:06:36 -0400

    can jump

--- ks-frontend/src/canvas.js
+++ ks-frontend/src/canvas.js
@@ -1,6 +1,6 @@
 let spr1;
-let GRAVITY = 1;
-let JUMP = 25; // how powerful is jump?
+let GRAVITY = 0.8;
+let JUMP = -5; // how powerful is jump?
 
 const p1 = {
   x: 400,
@@ -14,7 +14,8 @@ function setup() {
 
   spr1 = createSprite(
     p1.x, p1.y, p1.w, p1.h);
-  spr1.shapeColor = color(255);
+  spr1.shapeColor = color(255, 0, 0);
+  spr1.velocity.y = 0;
 }
 
 function draw() {
@@ -25,6 +26,25 @@ function draw() {
   text("use arrow keys, or SPACE to stop",
     width / 2, height * 0.67);
   drawSprites();
+  // spr1.velocity.y += GRAVITY;
+  if (spr1.position.y <= 300){
+    // spr1.velocity.y *= -1
+    // spr1.position.y = 350
+    spr1.velocity.y += GRAVITY;
+  }
+  else if (spr1.position.y >= 350){
+    spr1.velocity.y = 0
+  }
+
+  // if (spr1.y === 345) {
+  //   spr1.velocity.y = 1.5
+  //   if (spr1.y >= 350){
+  //     spr1.velocity.y = 0
+  //   }
+  // }
+  // else{
+  //   spr1.velocity.y = 0
+  // }
 }
 
 function keyPressed() {
@@ -32,10 +52,18 @@ function keyPressed() {
     spr1.setSpeed(3.5, 0);
   } else if (keyCode == LEFT_ARROW) {
     spr1.setSpeed(3.5, 180);
-  } else if (keyCode == UP_ARROW) {
-    console.log("jump")
-    // JUMP
-    // spr1.setSpeed(1.5, 270);
+  } else if (keyCode == UP_ARROW && spr1.position.y > 290) {
+    // p1.y -= 100
+    spr1.velocity.y = JUMP;
+    // spr1.velocity.y += GRAVITY;
+    // if (spr1.y === 345) {
+    //   spr1.velocity.y = 1.5
+    //   if (spr1.y >= 350){
+    //     spr1.velocity.y = 0
+    //   }
+    // }
+
+
 
   } else if (key == ' ') {
     spr1.setSpeed(0, 0);
