95cb7b9469e79b3db38f845407d9d7c99ea83347
github username <github email address>
2019-03-25 17:55:19 -0400

    prep for merge

--- ks-frontend/src/canvas.js
+++ ks-frontend/src/canvas.js
@@ -33,7 +33,7 @@ function setup() {
   spr1.velocity.y = 0;
 
   platformSpr = createSprite(platform1.x, 330, 70, 20)
-  spr1.shapeColor = color(0, 200, 0)
+  // spr1.shapeColor = color(0, 200, 0)
 
 }
 
@@ -55,15 +55,16 @@ function draw() {
 
   if (spr1.position.y <= 330) {
     spr1.velocity.y += GRAVITY;
-  } else if (spr1.position.y >= 390) {
+  }
+  else if (spr1.position.y >= 390) {
     spr1.velocity.y = 0
   }
+
+
   fill(100);
-
-
   if (platformRight === true){
     if (platform1.x >= 810){
-      platformRight = false;
+      // platformRight = false;
     }
     else {
       platform1.x += 0.5;
@@ -77,20 +78,23 @@ function draw() {
       platform1.x -= 0.5
     }
   }
+
   spr1.collide(platformSpr)
   platformSpr.collide(spr1)
 }
 
 function keyPressed() {
   if (keyCode == RIGHT_ARROW) {
-    spr1.setSpeed(3.5, 0);
+    spr1.position.x += 5
+
+    // spr1.setSpeed(3.5, 0);
   } else if (keyCode == LEFT_ARROW) {
-    spr1.setSpeed(3.5, 180);
+    // spr1.setSpeed(3.5, 180);
   } else if (keyCode == UP_ARROW && spr1.position.y > 330) {
     // p1.y -= 100
     spr1.velocity.y = JUMP;
   } else if (key == ' ') {
-    spr1.setSpeed(0, 0);
+    // spr1.setSpeed(0, 0);
   }
   return false;
 
