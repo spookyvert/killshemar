90cac7377ce1f68978d2566a2523f07260def3b2
Shemar Dacosta <dshemar6@gmail.com>
2019-04-09 14:40:18 -0400

    .

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
