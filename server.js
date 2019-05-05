0233a9d1f6dbfc9b933b24629541e96a89c55879
Shemar Dacosta <dshemar6@gmail.com>
2019-05-05 00:54:46 -0400

    .

--- server.js
+++ server.js
@@ -30,6 +30,25 @@ let express = require('express'),
 	app = express.createServer(express.logger()),
 	io = require('socket.io').listen(app)
 
+app.configure(function() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function() {
+	app.use(express.errorHandler({
+		dumpExceptions: true,
+		showStack: true
+	}));
+});
+
+app.configure('production', function() {
+	app.use(express.errorHandler());
+});
 
 io.configure(function() {
 	io.set("transports", ["xhr-polling"]);
