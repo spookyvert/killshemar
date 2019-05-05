bfb4c182fae29e8316b95e7a12299f6e74dd939a
Shemar Dacosta <dshemar6@gmail.com>
2019-05-04 20:26:46 -0400

    .

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ const server = express()
 
 const io = socketIO(server);
 
+// Heroku won't actually allow us to use WebSockets
+// so we have to setup polling instead.
+// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 
 //  initalliy setting both players to false, meaning they arent set yet
 
