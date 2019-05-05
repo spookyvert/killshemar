d7d4c410c4ae614fecdcf42d07e4e8324b0b1754
Shemar Dacosta <dshemar6@gmail.com>
2019-05-05 01:00:18 -0400

    .

--- server.js
+++ server.js
@@ -8,52 +8,27 @@
 //
 // server.listen(3000);
 
-// const express = require('express');
-// const socketIO = require('socket.io');
+const express = require('express');
+
 // const path = require('path');
-//
+
 // const PORT = process.env.PORT || 8000;
 // const INDEX = path.join(__dirname, 'index.html');
-//
-// const server = express()
-// 	.use((req, res) => res.sendFile(INDEX))
-// 	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-//
-// const io = socketIO(server);
+
+const app = express();
+const socketIO = require('socket.io');
+const server = express()
+	.use(app)
+	.listen(PORT, () => console.log(`Listening Socket on ${ PORT }`));
+
+const io = socketIO(server);
 
 // Heroku won 't actually allow us to use WebSockets
 // so we have to setup polling instead.
 // https: //devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
 //
 
-let express = require('express'),
-	app = express.createServer(express.logger()),
-	io = require('socket.io').listen(app)
 
-app.configure(function() {
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'ejs');
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function() {
-	app.use(express.errorHandler({
-		dumpExceptions: true,
-		showStack: true
-	}));
-});
-
-app.configure('production', function() {
-	app.use(express.errorHandler());
-});
-
-io.configure(function() {
-	io.set("transports", ["xhr-polling"]);
-	io.set("polling duration", 10);
-});
 
 
 //  initalliy setting both players to false, meaning they arent set yet
